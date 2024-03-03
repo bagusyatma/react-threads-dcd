736306566496f5adde1c44ed15d778759da2e9a3
bagusyatma <42886214+bagusyatma@users.noreply.github.com>
2024-03-03 10:39:27 +0700

    create error to testing ci/cd

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -3,11 +3,7 @@ import { useState } from 'react';
 function useInput(defaultValue = '') {
   const [value, setValue] = useState(defaultValue);
 
-  function handleValueChange({ target }) {
-    setValue(target.value);
-  }
-
-  return [value, handleValueChange, setValue];
+  return [value, setValue];
 }
 
 export default useInput;
